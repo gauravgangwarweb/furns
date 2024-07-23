55d30d06b38c2216c0fafd6ca8ba1721b6674813
gauravgangwarweb <gauravgangwar501@gmail.com>
2024-07-23 16:36:17 +0530

    refactor: Update OurProduct component with new buttons and spacing

--- src/components/home/OurProduct.jsx
+++ src/components/home/OurProduct.jsx
@@ -3,7 +3,12 @@ const OurProduct = () => {
         <div className="mt-28">
             <div className="flex flex-col items-center justify-center">
                 <h4 className="text-4xl font-bold raleway">Our Products</h4>
-                <p className="px-[400px] text-center text-base mt-4">Explore our exquisite furniture collection, combining timeless elegance with modern functionality to enhance every space.</p>
+                <p className="px-[400px] text-center text-base mt-6">Explore our exquisite furniture collection, combining timeless elegance with modern functionality to enhance every space.</p>
+                <div className="flex gap-8 mt-8">
+                    <button className="text-lg font-semibold">New Arrival</button>
+                    <button className="text-lg font-semibold">Top Product</button>
+                    <button className="text-lg font-semibold">Top Rated</button>
+                </div>
             </div>
         </div>
     );
